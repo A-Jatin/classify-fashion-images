60059d82b51ebbb7f8ff8caefa4aa23c25ea1e26
Jatin Ahuja <aahuja.jatin@gmail.com>
2022-02-04 10:10:29 +0530

    Update user.py

--- src/user.py
+++ src/user.py
@@ -2,13 +2,13 @@ import sys
 from orchestration.factory import MessageBrokerFactory
 
 
-def user(path):
+def user(path, broker):
 
-    broker = MessageBrokerFactory().build('apache')
+    broker = MessageBrokerFactory().build(broker)
     broker.produce(topic='server', data=path)
-    results = broker.consume(topic='user')
+    results = broker.consume(topic='user', subscription_name='user')
     print(results)
 
 
 if __name__ == '__main__':
-    user(path=sys.argv[1])
+    user(path=sys.argv[1], broker=sys.argv[2])
