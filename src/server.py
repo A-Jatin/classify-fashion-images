9d0ef6cd9ba724bbe63663ed380508d835cb75ce
Jatin Ahuja <aahuja.jatin@gmail.com>
2022-02-04 10:11:07 +0530

    Update server.py

--- src/server.py
+++ src/server.py
@@ -5,14 +5,14 @@ from orchestration.factory import MessageBrokerFactory
 from run_image_classifier import run
 
 
-def server():
+def server(broker):
     #os.system(kafka_path+"/bin/kafka-topics.sh --create --topic user --bootstrap-server localhost:9092")
     #os.system(kafka_path+"/bin/kafka-topics.sh --create --topic server --bootstrap-server localhost:9092")
-    broker = MessageBrokerFactory().build('apache', subscription_name = 'server;)
+    broker = MessageBrokerFactory().build(broker, subscription_name = 'server;)
     path = broker.consume(topic='user')
     labels = run(path)
     broker.produce(topic='server', data=labels)
 
 
 if __name__ == '__main__':
-    server()
+    server(broker=sys.argv[1])
